54a3f54167fd3d5890e9310693e4c2183f2655fa
Ivander Jonathan <ivanderjmw@gmail.com>
2020-02-08 02:49:10 +0800

    Version 1.1

--- E2_Assignment_G5.py
+++ E2_Assignment_G5.py
@@ -11,36 +11,64 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
-servo_pin = 20
-solenoid_pin = 21
+#servo_pin = 20
+#solenoid_pin = 21
 
-
-GPIO.setup(servo_pin, GPIO.OUT)
-GPIO.setup(solenoid_pin, GPIO.OUT)
-
-p = GPIO.PWM(servo_pin, 50)
-
-p.start (7.5)
+class servo:
+    
+    # Initializer / Instance attributes
+    def __init__(self, pinnum):
+        self.pin = pinnum
+    
+    def start (self):
+        GPIO.setup(self.pin, GPIO.OUT)
+        global p 
+        p = GPIO.PWM(self.pin, 50)
+        p.start (7.5)
+        print(p)
         
-def servodothething ():
-    x = input(int)
-    if x<0 or x>180:
-            print ("wrong value fuck")
-            return
-    else:
-            p.ChangeDutyCycle (2.5 + (5 * x/90))
-            time.sleep (1)
+    def move (self, x):
+        global p
+        print(p)
+        if x<0 or x>180:
+                print ("wrong value fuck")
+                return
+        else:
+                p.ChangeDutyCycle (2.5 + (5 * x/90))
+                time.sleep (1)
+                return
+            
+class solenoid:
+    
+    # Initializer / Instance attributes
+    def __init__(self, pinnum):
+        self.pin = pinnum
+    
+    def start (self):
+        GPIO.setup(self.pin, GPIO.OUT)
+        return
+        
+    def toggle (self, toggle_time):
+        GPIO.output(self.pin, 1)
+        print("GPIO HIGH (on)")
+        time.sleep(toggle_time)
+        GPIO.output(self.pin, 0)
+        print("GPIO HIGH (off)")
+        time.sleep(toggle_time)
+        return
+
+# Initialize the two components
+servo = servo(16)
+servo.start()
+solenoid = solenoid(21)
+solenoid.start()
 
 
 try:
     while True:
-        servodothething();
-        GPIO.output(solenoid_pin, 1)
-        print("GPIO HIGH (on)")
-        time.sleep(1)
-        GPIO.output(solenoid_pin, 0)
-        print("GPIO HIGH (off)")
-        time.sleep(1)
+        servo.move(input(int))
+        solenoid.toggle(.5)
+        
         
 except KeyboardInterrupt:
     p.stop()
