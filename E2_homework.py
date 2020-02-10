fb54981d3f4719fbcdcb9e991152aa700d5bd5ef
ivanderjmw <57558909+ivanderjmw@users.noreply.github.com>
2020-02-10 16:13:00 +0800

    Servo and solenoid ok

--- E2_homework.py
+++ E2_homework.py
@@ -46,11 +46,13 @@ def scanner():
 def servo_setup(servo_pin): #set up servo motor
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(servo_pin, GPIO.OUT)
+    global p
     p = GPIO.PWM(servo_pin, 50)
     p.start(2.5)
 
 def rotation(angle): #rotate servo 
     dc = angle/18 +2.5 #convert angle to duty cycle
+    global p
     p.ChangeDutyCycle(dc) #rotate motor to needed angle 
     time.sleep(0.1)
 
@@ -59,26 +61,33 @@ def solenoid_setup(solenoid_pin): #setup solenoid
     GPIO.setup(solenoid_pin, GPIO.OUT) 
 
 def solenoid_punch(n): #make solenoid punch n times
-    for i in range(1,n):
+    for i in range(0,n):
         GPIO.output(solenoid_pin, 1)
         time.sleep(0.2)
         GPIO.output(solenoid_pin, 0)
         time.sleep(0.2)
-        
+
 try:
     servo_setup(servo_pin)
     solenoid_setup(solenoid_pin)
     while True: 
-        if scanner() == 1:
-            rotation(45)
-            solenoid_punch(1)
-        else:
-            continue 
+        #if scanner() == 1:
+        rotation(45)
+        solenoid_punch(1)
+        rotation(0)
+        time.sleep(1)
+        #scanner()
+        #else:
+        #    continue 
 except rospy.ROSInterruptException:
     p.stop()
     GPIO.cleanup()
-            
-        
+except KeyboardInterrupt:
+    p.stop()
+    GPIO.cleanup()
+
+
+
         
         
     
