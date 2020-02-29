27ca262a2525ed15588d64cbe42df7fd5cb6f8cb
Shih-Cheng Yen <yenshihcheng@gmail.com>
2020-02-29 22:02:48 +0800

    Simplified code to make laser_range be a 1-dimensional vector.

--- scanner.py
+++ scanner.py
@@ -7,15 +7,15 @@ from sensor_msgs.msg import LaserScan
 
 def callback(msg):
 	# create numpy array
-	laser_range = np.array([msg.ranges])
+	laser_range = np.array(msg.ranges)
 	# replace 0's with nan
 	lr2 = laser_range
 	lr2[lr2==0] = np.nan
 	# find index with minimum value
 	lr2i = np.nanargmin(lr2)
-	
+
 	# log the info
-    	rospy.loginfo('Shortest distance is %i degrees', lr2i)
+    	rospy.loginfo('Shortest distance at %i degrees', lr2i)
 
 
 def scanner():
@@ -27,7 +27,7 @@ def scanner():
 
 	# subscribe to LaserScan data
 	rospy.Subscriber('scan', LaserScan, callback)
-    
+
 	# wait until it is time to run again
 	rate.sleep()
 
