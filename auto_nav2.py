e9732cdd21e7201899a2ede2d9230c1d5211fb78
rubberdinghy <60140829+rubberdinghy@users.noreply.github.com>
2020-03-10 18:25:40 +0800

    Added code to play a sound when the mapping is complete.

--- auto_nav2.py
+++ auto_nav2.py
@@ -10,9 +10,12 @@ import math
 import cmath
 import numpy as np
 import time
+import cv2
+from sound_play.msg import SoundRequest
+from sound_play.libsoundplay import SoundClient
 
 laser_range = np.array([])
-occdata = []
+occdata = np.array([])
 yaw = 0.0
 rotate_speed = 0.1
 linear_speed = 0.1
@@ -45,13 +48,18 @@ def get_occupancy(msg):
     global occdata
 
     # create numpy array
-    occdata = np.array(msg.data)
+    msgdata = np.array(msg.data)
     # compute histogram to identify percent of bins with -1
-    occ_counts = np.histogram(occdata,occ_bins)
+    occ_counts = np.histogram(msgdata,occ_bins)
     # calculate total number of bins
     total_bins = msg.info.width * msg.info.height
     # log the info
     rospy.loginfo('Unmapped: %i Unoccupied: %i Occupied: %i Total: %i', occ_counts[0][0], occ_counts[0][1], occ_counts[0][2], total_bins)
+
+    # make msgdata go from 0 instead of -1, reshape into 2D
+    oc2 = msgdata + 1
+    # reshape to 2D array using column order
+    occdata = np.uint8(oc2.reshape(msg.info.height,msg.info.width,order='F'))
 
 
 def rotatebot(rot_angle):
@@ -158,6 +166,56 @@ def stopbot():
     pub.publish(twist)
 
 
+def closure(mapdata):
+    # This function checks if mapdata contains a closed contour. The function
+    # assumes that the raw map data from SLAM has been modified so that
+    # -1 (unmapped) is now 0, and 0 (unoccupied) is now 1, and the occupied
+    # values go from 1 to 101.
+
+    # According to: https://stackoverflow.com/questions/17479606/detect-closed-contours?rq=1
+    # closed contours have larger areas than arc length, while open contours have larger
+    # arc length than area. But in my experience, open contours can have areas larger than
+    # the arc length, but closed contours tend to have areas much larger than the arc length
+    # So, we will check for contour closure by checking if any of the contours
+    # have areas that are more than 10 times larger than the arc length
+    # This value may need to be adjusted with more testing.
+    ALTHRESH = 10
+    # We will slightly fill in the contours to make them easier to detect
+    DILATE_PIXELS = 3
+
+    # assumes mapdata is uint8 and consists of 0 (unmapped), 1 (unoccupied),
+    # and other positive values up to 101 (occupied)
+    # so we will apply a threshold of 2 to create a binary image with the
+    # occupied pixels set to 255 and everything else is set to 0
+    # we will use OpenCV's threshold function for this
+    ret,img2 = cv2.threshold(mapdata,2,255,0)
+    # we will perform some erosion and dilation to fill out the contours a
+    # little bit
+    element = cv2.getStructuringElement(cv2.MORPH_CROSS,(DILATE_PIXELS,DILATE_PIXELS))
+    # img3 = cv2.erode(img2,element)
+    img4 = cv2.dilate(img2,element)
+    # use OpenCV's findContours function to identify contours
+    fc = cv2.findContours(img4, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contours = fc[0]
+    # find number of contours returned
+    lc = len(contours)
+    # create array to compute ratio of area to arc length
+    cAL = np.zeros((lc,2))
+    for i in range(lc):
+        cAL[i,0] = cv2.contourArea(contours[i])
+        cAL[i,1] = cv2.arcLength(contours[i], True)
+
+    # closed contours tend to have a much higher area to arc length ratio,
+    # so if there are no contours with high ratios, we can safely say
+    # there are no closed contours
+    cALratio = cAL[:,0]/cAL[:,1]
+    # print(cALratio)
+    if np.any(cALratio > ALTHRESH):
+        return True
+    else:
+        return False
+
+
 def mover():
     global laser_range
 
@@ -174,13 +232,18 @@ def mover():
 
     rate = rospy.Rate(5) # 5 Hz
 
+    # save start time to file
+    start_time = time.time()
+    # initialize variable to write elapsed time to file
+    timeWritten = 0
+
     # find direction with the largest distance from the Lidar,
     # rotate to that direction, and start moving
     pick_direction()
 
     while not rospy.is_shutdown():
         if laser_range.size != 0:
-            # check distances in front of TurtleBot and find values less 
+            # check distances in front of TurtleBot and find values less
             # than stop_distance
             lri = (laser_range[front_angles]<float(stop_distance)).nonzero()
             rospy.loginfo('Distances: %s', str(lri))
@@ -194,6 +257,22 @@ def mover():
             # rotate to that direction
             # start moving
             pick_direction()
+
+        # check if SLAM map is complete
+        if timeWritten :
+            if closure(occdata) :
+                # map is complete, so save current time into file
+                with open("maptime.txt", "w") as f:
+                    f.write("Elapsed Time: " + str(time.time() - start_time))
+                timeWritten = 1
+                # play a sound
+                soundhandle = SoundClient()
+                rospy.sleep(1)
+                soundhandle.stopAll()
+                soundhandle.play(SoundRequest.NEEDS_UNPLUGGING)
+                rospy.sleep(2)
+                # save the map
+                cv2.imwrite('mazemap.png',occdata)
 
         rate.sleep()
 
