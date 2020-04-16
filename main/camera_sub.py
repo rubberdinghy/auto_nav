d92969a198888d08e8428e614ccad9aa0c26f8c1
Juin Hwaye, the Legend <33744207+felixjhmong@users.noreply.github.com>
2020-04-16 19:20:24 +0800

    Update camera_sub.py

--- main/camera_sub.py
+++ main/camera_sub.py
@@ -1,8 +1,7 @@
 # import the necessary packages
 from webcamvideostream import WebcamVideoStream
 class VideoStream:
-    def __init__(self, src=0, usePiCamera=False, resolution=(320, 240),
-		framerate=32):
+	def __init__(self, src=0, usePiCamera=False, resolution=(320, 240), framerate=32):
 		# check to see if the picamera module should be used
 		if usePiCamera:
 			# only import the picamera packages unless we are
@@ -12,14 +11,13 @@ class VideoStream:
 			from pivideostream import PiVideoStream
 			# initialize the picamera stream and allow the camera
 			# sensor to warmup
-			self.stream = PiVideoStream(resolution=resolution,
-				framerate=framerate)
+			self.stream = PiVideoStream(resolution=resolution, framerate=framerate)
 		# otherwise, we are using OpenCV so initialize the webcam
 		# stream
 		else:
 			self.stream = WebcamVideoStream(src=src)
             
-    def start(self):
+	def start(self):
 		# start the threaded video stream
 		return self.stream.start()
 	def update(self):
