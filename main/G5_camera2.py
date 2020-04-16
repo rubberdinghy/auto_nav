f66510eddfda4fdc61427a93b21fac27d3b149de
Juin Hwaye, the Legend <33744207+felixjhmong@users.noreply.github.com>
2020-04-16 21:24:46 +0800

    Update G5_camera2.py

--- main/G5_camera2.py
+++ main/G5_camera2.py
@@ -17,8 +17,6 @@ import imutils
 import sys
 
 
-
-
 def talker():
 	rospy.init_node('ball_tracking1', anonymous=True)
 	pub=rospy.Publisher('coordinates',Float32,queue_size=1)
@@ -28,11 +26,11 @@ def talker():
 	ap.add_argument("-b", "--buffer", type=int, default=64,help="max buffer size")
 	args = vars(ap.parse_args())
 
-	greenLower = (155, 103, 82)
-	greenUpper = (178, 255, 255)
+	greenLower = (29, 86, 6)
+	greenUpper = (64, 255, 255)
 	pts = deque(maxlen=64)
 	if not args.get("video", False):
-		camera = cv2.VideoCapture()
+		camera = cv2.VideoCapture(0)
 	else:
 		camera = cv2.VideoCapture('args["video"]')
 
@@ -71,6 +69,8 @@ def talker():
 			break
 
 	rospy.spin()
+
+
     
 if __name__ == '__main__':
     try:
