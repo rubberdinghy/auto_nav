7121260d659d1600bc864376bc0081de7497237c
Juin Hwaye, the Legend <33744207+felixjhmong@users.noreply.github.com>
2020-04-16 16:17:25 +0800

    Update camera3.py

--- main/camera3.py
+++ main/camera3.py
@@ -1,7 +1,9 @@
 import cv2
 import numpy as np 
+import rospy
 from picamera.array import PiRGBArray
 from picamera import PiCamera 
+
 
 def nothing(x):
     pass
@@ -32,6 +34,22 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	upperLimit = np.uint8([hsvGreen[0][0][0]+10,255,255])
 
 	mask = cv2.inRange(hsv, lowerLimit, upperLimit)
+	mask = cv2.erode(mask, None, iterations=2)
+	mask = cv2.dilate(mask, None, iterations=2)
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
+	center = None
+	if len(cnts) > 0:
+		c = max(cnts, key=cv2.contourArea)
+		((x, y), radius) = cv2.minEnclosingCircle(c)
+		M = cv2.moments(c)
+		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+		#print(center)
+		pub.publish(int(M["m01"] / M["m00"]))
+		rospy.loginfo(int(M["m01"] / M["m00"]))
+		rate.sleep()
+		if radius > 10:
+			cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
+			cv2.circle(frame, center, 5, (0, 0, 255), -1)
 
 	result = cv2.bitwise_and(image	, image	, mask=mask)
 
