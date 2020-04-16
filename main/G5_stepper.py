c3c12da346e6622a8257fbc42d1fd33337f9a5f7
felixjhmong <felixjhmong@gmail.com>
2020-04-17 02:27:08 +0800

    Added controls to stepper motor tests.

--- main/G5_stepper.py
+++ main/G5_stepper.py
@@ -1,55 +1,131 @@
+#!/usr/bin/env python
+# coding: latin-1
+# Ingmar Stapel
+# Date: 20170615
+# Version Alpha 0.1
+# Decision Maker
+
+from time import sleep
 import RPi.GPIO as GPIO
-import time
- 
+import os
+#from random import randint
+import random
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-coil_A_1_pin = 25 # blue
-coil_A_2_pin = 8 # orange
-coil_B_1_pin = 7 # blue
-coil_B_2_pin = 1 # yellow
- 
-# adjust if different
-StepCount = 8
-Seq = range(0, StepCount)
-Seq[0] = [0,1,0,0]
-Seq[1] = [0,1,0,1]
-Seq[2] = [0,0,0,1]
-Seq[3] = [1,0,0,1]
-Seq[4] = [1,0,0,0]
-Seq[5] = [1,0,1,0]
-Seq[6] = [0,0,1,0]
-Seq[7] = [0,1,1,0]
- 
-GPIO.setup(enable_pin, GPIO.OUT)
-GPIO.setup(coil_A_1_pin, GPIO.OUT)
-GPIO.setup(coil_A_2_pin, GPIO.OUT)
-GPIO.setup(coil_B_1_pin, GPIO.OUT)
-GPIO.setup(coil_B_2_pin, GPIO.OUT)
- 
-GPIO.output(enable_pin, 1)
- 
-def setStep(w1, w2, w3, w4):
-    GPIO.output(coil_A_1_pin, w1)
-    GPIO.output(coil_A_2_pin, w2)
-    GPIO.output(coil_B_1_pin, w3)
-    GPIO.output(coil_B_2_pin, w4)
- 
-def forward(delay, steps):
-    for i in range(steps):
-        for j in range(StepCount):
-            setStep(Seq[j][0], Seq[j][1], Seq[j][2], Seq[j][3])
-            time.sleep(delay)
- 
-def backwards(delay, steps):
-    for i in range(steps):
-        for j in reversed(range(StepCount)):
-            setStep(Seq[j][0], Seq[j][1], Seq[j][2], Seq[j][3])
-            time.sleep(delay)
- 
-if __name__ == '__main__':
-    while True:
-        delay = raw_input("Time Delay (ms)?")
-        steps = raw_input("How many steps forward? ")
-        forward(int(delay) / 1000.0, int(steps))
-        steps = raw_input("How many steps backwards? ")
-        backwards(int(delay) / 1000.0, int(steps))
+
+# Verwendete Pins des ULN2003A auf die Pins des Rapberry Pi
+# zugeordnet 
+IN1=25 # IN1
+IN2=8 # IN2
+IN3=7 # IN3
+IN4=2 # IN4
+
+# Wartezeit regelt die Geschwindigkeit wie schnell sich der Motor
+# dreht.
+time = 0.001
+
+# Pins aus Ausgänge definieren
+GPIO.setup(IN1,GPIO.OUT)
+GPIO.setup(IN2,GPIO.OUT)
+GPIO.setup(IN3,GPIO.OUT)
+GPIO.setup(IN4,GPIO.OUT)
+# Alle Pins werden initial auf False gesetzt. So dreht sich der 
+# Stepper-Motor nicht sofort irgendwie.
+GPIO.output(IN1, False)
+GPIO.output(IN2, False)
+GPIO.output(IN3, False)
+GPIO.output(IN4, False)
+
+# Der Schrittmotoren 28BYJ-48 ist so aufgebaut, das der Motor im
+# Inneren 8 Schritte für eine Umdrehung benötigt. Durch die Betriebe
+# benätigt es aber 512 x 8 Schritte damit die Achse sich einmal um
+# sich selbt also 360° dreht.
+
+# Definition der Schritte 1 - 8 über die Pins IN1 bis IN4
+# Zwischen jeder Bewegung des Motors wird kurz gewartet damit der
+# Motoranker seine Position erreicht.
+def Step1():
+    GPIO.output(IN4, True)
+    sleep (time)
+    GPIO.output(IN4, False)
+
+def Step2():
+    GPIO.output(IN4, True)
+    GPIO.output(IN3, True)
+    sleep (time)
+    GPIO.output(IN4, False)
+    GPIO.output(IN3, False)
+
+def Step3():
+    GPIO.output(IN3, True)
+    sleep (time)
+    GPIO.output(IN3, False)
+
+def Step4():
+    GPIO.output(IN2, True)
+    GPIO.output(IN3, True)
+    sleep (time)
+    GPIO.output(IN2, False)
+    GPIO.output(IN3, False)
+
+def Step5():
+    GPIO.output(IN2, True)
+    sleep (time)
+    GPIO.output(IN2, False)
+
+def Step6():
+    GPIO.output(IN1, True)
+    GPIO.output(IN2, True)
+    sleep (time)
+    GPIO.output(IN1, False)
+    GPIO.output(IN2, False)
+
+def Step7():
+    GPIO.output(IN1, True)
+    sleep (time)
+    GPIO.output(IN1, False)
+
+def Step8():
+    GPIO.output(IN4, True)
+    GPIO.output(IN1, True)
+    sleep (time)
+    GPIO.output(IN4, False)
+    GPIO.output(IN1, False)
+
+# Umdrehung links herum  
+def left(step):	
+	for i in range (step):   
+		#os.system('clear') # verlangsamt die Bewegung des Motors zu sehr. 
+		Step1()
+		Step2()
+		Step3()
+		Step4()
+		Step5()
+		Step6()
+		Step7()
+		Step8()  
+		print "Step left: ",i
+
+# Umdrehung rechts herum		
+def right(step):
+	for i in range (step):
+		#os.system('clear') # verlangsamt die Bewegung des Motors zu sehr.  
+		Step8()
+		Step7()
+		Step6()
+		Step5()
+		Step4()
+		Step3()
+		Step2()
+		Step1()  
+		print "Step right: ",i	
+		
+# Hier faellt die Entscheidung ob links oder rechts herum
+
+while True:
+    dir = str(input('Enter rotation direction (L/R):'))
+    if dir == 'L':
+        left(input('value:'))
+    elif dir == 'R':
+	    right(input('value:'))
+GPIO.cleanup()
